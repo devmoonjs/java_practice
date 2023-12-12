2d3a74eacbd963d0c92c0b73a0d469a013f5fc9e
MOON <moonjs1018@naver.com>
2023-12-12 11:03:34 +0900

    first commit

---
+++ src/org/opentutorials/javatutorials/number/Number.java
@@ -0,0 +1,9 @@
+package org.opentutorials.javatutorials.number;
+
+public class Number {
+
+	public static void main(String[] args) {
+		System.out.println(2);
+	}
+
+}
---
+++ src/org/opentutorials/javatutorials/variables/StringDemo.java
@@ -0,0 +1,14 @@
+package org.opentutorials.javatutorials.variables;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String first = "This is";
+		String second = "a concatenated";
+		String third = first + second;
+		System.out.println(third);
+	
+
+	}
+
+}
