79a02dfd5f0396043a7b0972613c5a23c76feb77
moonjs1018@naver.com <moonjs1018@naver.com>
2023-12-13 14:22:50 +0900

    git commit issue test

--- src/org/opentutorials/javatutorials/overloading/example1/CalculatorDemo.java
+++ src/org/opentutorials/javatutorials/overloading/example1/CalculatorDemo.java
@@ -1,9 +1,39 @@
 package org.opentutorials.javatutorials.overloading.example1;
 
+class Calculator{
+	int left, int right;
+	int third = 0;
+	
+	public void setOprands(int left, int right) {
+		this.left = left;
+		this.right = right;
+	}
+	
+	public void setOprands(int left, int right, int third) {
+		this.left = left;
+		this.right = right;
+		this.third = third;
+	}
+	
+	public void sum() {
+		System.out.println(this.left + this.right + this.third){
+			
+		}
+	}
+}
+
 public class CalculatorDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Calculator c1 = new Calculator();
+		c1.setOprands(10,20);
+		c1.sum();
+		c1.avg();
+		c1.setOprands(10,20,30);
+		c1.sum();
+		c1.avg();
+		// commit issue test
 
 	}
 
