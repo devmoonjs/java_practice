155e5aae629f72ac11d55eb098f2ba66a9c3a5f5
MOON <moonjs1018@naver.com>
2023-12-17 15:55:40 +0900

    Enum practice

---
+++ src/org/opentutorials/javatutorials/exception/ConstantDemo.java
@@ -0,0 +1,27 @@
+package org.opentutorials.javatutorials.exception;
+
+enum Fruit{
+	APPLE("red"), PEACH("pink"), BANANA("yellow");
+	private String color;
+	
+	Fruit(String color){
+		System.out.println("Call Constructor " + this);
+		this.color = color;
+	}
+	String getColor() {
+		return this.color;
+	}
+}
+
+enum company{
+	GOOGLE, APPLE, ORACLE;
+}
+
+public class ConstantDemo {
+
+		public static void main(String[] args) {
+			for (Fruit f : Fruit.values()) {
+				System.out.println(f + ", " + f.getColor());
+			}
+		}
+}
