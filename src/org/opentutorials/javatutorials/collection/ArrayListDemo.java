177c9f315d0bf9a4f99c74134ea855c4ebb1706c
MOON <moonjs1018@naver.com>
2023-12-19 13:52:59 +0900

    ArrayList practice

--- src/org/opentutorials/javatutorials/collection/ArrayListDemo.java
+++ src/org/opentutorials/javatutorials/collection/ArrayListDemo.java
@@ -1,19 +1,42 @@
 package org.opentutorials.javatutorials.collection;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.HashSet;
 
 
 public class ArrayListDemo {
 
 	public static void main(String[] args) {
-		ArrayList<Object> a1 = new ArrayList<Object>();
+		HashSet<Object> a1 = new HashSet<Object>();
+		ArrayList<Object> a2 = new ArrayList<Object>();
+		
 		a1.add("one");
 		a1.add("two");
 		a1.add("three");
+		a1.add("three");
+		a1.add("three");
+		a1.add("three");
+		a1.add("ten");
 		
-		for(int i=0; i<a1.size(); i++){
-				System.out.println(a1.get(i));
+		a2.add("one");
+		a2.add("two");
+		a2.add("three");
+		a2.add("three");
+		a2.add("three");
+		a2.add("three");
+		a2.add("ten");
+			
+		Iterator<Object> it = a1.iterator();
+		while(it.hasNext()) {
+			String str = (String)it.next();
+			System.out.println(str);
 		}
+		
+		Iterator<Object> it2 = a2.iterator();
+		while(it2.hasNext()) {
+			String str2 = (String)it2.next();
+			System.out.println(str2);
+		}			
 	}
-
 }
