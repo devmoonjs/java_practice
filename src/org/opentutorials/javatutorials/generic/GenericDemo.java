591774ca66a2b97e7ae965acd6a0b00d0e86de3a
MOON <moonjs1018@naver.com>
2023-12-19 11:04:19 +0900

    Generic practice

---
+++ src/org/opentutorials/javatutorials/generic/GenericDemo.java
@@ -0,0 +1,26 @@
+package org.opentutorials.javatutorials.generic;
+
+class EmployeeInfo{
+	public int rank;
+	EmployeeInfo(int rank){
+		this.rank = rank;
+	}
+}
+class Person<T, S>{
+	public T info;
+	public S id;
+	Person(T info, S id) {
+		this.info = info;
+		this.id = id;
+	}
+}
+
+public class GenericDemo {
+
+	public static void main(String[] args) {
+		Integer id = new Integer(1);
+		Person<EmployeeInfo, Integer> p1 = new Person<EmployeeInfo, Integer>(new EmployeeInfo(1), id);
+		System.out.println(p1.id.intValue());
+	}
+
+}
